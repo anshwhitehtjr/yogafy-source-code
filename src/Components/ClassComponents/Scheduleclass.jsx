3de95e37aa75ac70fae1c7a7c986778e1478cf4f
anshwhitehtjr <kalpaiteansh@gmail.com>
2021-11-28 10:28:44 +0530

    Last Commit

--- src/Components/ClassComponents/Scheduleclass.jsx
+++ src/Components/ClassComponents/Scheduleclass.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Scheduleclass = () => {
    return (
       <>
          <div className="container">
-            <h1>Schedule Classes</h1>
+            <form class="form-floating">
+               <input hidden type="text" className="form-control" id="title" placeholder="This is the title" value="This is a title" />
+               <label htmlFor="floatingInputValue">Input with value</label>
+            </form>
          </div>
       </>
-   )
-}
+   );
+};
 
-export default Scheduleclass
+export default Scheduleclass;
